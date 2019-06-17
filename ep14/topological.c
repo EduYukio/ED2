d6c92e0cc458f4a8f458315d5dba27f7b154180b
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2019-06-16 23:34:39 -0300

    Add header and remove useless comments

--- ep14/topological.c
+++ ep14/topological.c
@@ -1,3 +1,42 @@
+/****************************************************************
+    Nome: Eduardo Yukio Rodrigues
+    NUSP: 8988702
+
+    Ao preencher esse cabeçalho com o meu nome e o meu número USP,
+    declaro que todas as partes originais desse exercício programa (EP)
+    foram desenvolvidas e implementadas por mim e que portanto não 
+    constituem desonestidade acadêmica ou plágio.
+    Declaro também que sou responsável por todas as cópias desse
+    programa e que não distribui ou facilitei a sua distribuição.
+    Estou ciente que os casos de plágio e desonestidade acadêmica
+    serão tratados segundo os critérios divulgados na página da 
+    disciplina.
+    Entendo que EPs sem assinatura devem receber nota zero e, ainda
+    assim, poderão ser punidos por desonestidade acadêmica.
+
+    Abaixo descreva qualquer ajuda que você recebeu para fazer este
+    EP.  Inclua qualquer ajuda recebida por pessoas (inclusive
+    monitoras e colegas). Com exceção de material de MAC0323, caso
+    você tenha utilizado alguma informação, trecho de código,...
+    indique esse fato abaixo para que o seu programa não seja
+    considerado plágio ou irregular.
+
+    Exemplo:
+
+        A monitora me explicou que eu devia utilizar a função xyz().
+
+        O meu método xyz() foi baseada na descrição encontrada na 
+        página https://www.ime.usp.br/~pf/algoritmos/aulas/enumeracao.html.
+
+    Descrição de ajuda ou indicação de fonte:
+
+
+
+    Se for o caso, descreva a seguir 'bugs' e limitações do seu programa:
+        Este EP foi feito em C99
+
+****************************************************************/
+
 /*
  * MAC0323 Algoritmos e Estruturas de Dados II
  * 
@@ -167,7 +206,6 @@ newTopological(Digraph G) {
     newTopo->rank = ecalloc(V, sizeof(int));
 
     if (!hasCycle(newTopo)) {
-        // freeBag(newTopo->order);
         newTopo->order = newTopo->invertedPostorder;
         int i = 0;
         for (int v = itens(newTopo->order, TRUE); v >= 0; v = itens(newTopo->order, FALSE)) {
@@ -404,7 +442,6 @@ static void dfsCheckCycle(Topological ts, Digraph G, int v) {
                 add(ts->cycle, x);
             }
             add(ts->cycle, w);
-            //add(ts->cycle, v);
         }
     }
     ts->onStack[v] = FALSE;
